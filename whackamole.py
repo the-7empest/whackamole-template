d905172055b31dd9fa556008f38935c1e1308b54
the-7empest <145264692+the-7empest@users.noreply.github.com>
2024-11-18 16:31:28 -0500

    Update whackamole.py

--- whackamole.py
+++ whackamole.py
@@ -1,24 +1,41 @@
 import pygame
-
-
+import random
+GRID_WIDTH, GRID_HEIGHT = 20, 16
+SQUARE_SIZE = 32
+WINDOW_WIDTH, WINDOW_HEIGHT = GRID_WIDTH * SQUARE_SIZE, GRID_HEIGHT * SQUARE_SIZE
+def draw_grid(screen):
+     for x in range(0, WINDOW_WIDTH, SQUARE_SIZE):
+        pygame.draw.line(screen, "black", (x, 0), (x, WINDOW_HEIGHT))
+    for y in range(0, WINDOW_HEIGHT, SQUARE_SIZE):
+        pygame.draw.line(screen, "black", (0, y), (WINDOW_WIDTH, y))
+def random_position():
+    grid_x = random.randrange(0, GRID_WIDTH) * SQUARE_SIZE
+    grid_y = random.randrange(0, GRID_HEIGHT) * SQUARE_SIZE
+    return grid_x, grid_y
 def main():
-    try:
-        pygame.init()
+    pygame.init()
         # You can draw the mole with this snippet:
         # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
-        mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((640, 512))
-        clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = pygame.time.Clock()
+    mole_image = pygame.image.load("mole.png")
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
-            pygame.display.flip()
-            clock.tick(60)
-    finally:
-        pygame.quit()
+                 elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:  # Left mouse button
+                        mouse_x, mouse_y = event.pos
+                        mole_x, mole_y = mole_position
+                    if mole_x <= mouse_x < mole_x + SQUARE_SIZE and mole_y <= mouse_y < mole_y + SQUARE_SIZE:
+                        mole_position = random_position()
+                screen.fill("light green")
+                draw_grid(screen)
+                screen.blit(mole_image, mole_image.get_rect(topleft=mole_position))
+                pygame.display.flip()
+                clock.tick(60)
+            pygame.quit()
 
 
 if __name__ == "__main__":
